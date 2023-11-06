900776e8e831cfe7e0279733cf4c79c7bbcf55ce
harry123001 <yello@Yellos-MacBook-Air.local>
2023-11-06 10:20:40 -0500

    Edits

--- react-life-cycle/src/App.js
+++ react-life-cycle/src/App.js
@@ -1,7 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [color, setColor] = useState()
+
+  const fetchColor = () => {
+    const r = Math.floor(Math.random() * 256)
+    const g = Math.floor(Math.random() * 256)
+    const b = Math.floor(Math.random() * 256)
+    fetch(`http://www.thecolorapi.com/id?rgb=${r},${g},${b}`)
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        setColor(res)
+    })
+}
+
   return (
     <div className="App">
       <h1>Hello World</h1>
