7aff9c8dfc4c815b1b23c4de778807f50cc4a4bc
harry123001 <yello@Yellos-MacBook-Air.local>
2023-11-06 09:48:00 -0500

    Finished

--- Lessons/Cards.js
+++ Lessons/Cards.js
@@ -48,4 +48,18 @@ class Card {
   
       this.shuffle()
     }
-  }
+    shuffle(){
+        const cards = this.cards
+    
+        for (let i = cards.length - 1; i > 0; i--){
+          const j = Math.floor(Math.random() * (i + 1))
+          // [cards[i], cards[j]] = [cards[j], cards[i]]
+          let temp = cards[i]
+          cards[i] = cards[j]
+          cards[j] = temp
+        }
+      }
+    }
+    
+    const gameDeck = new Deck()
+    console.log(gameDeck.cards)
