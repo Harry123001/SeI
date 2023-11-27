0057d28f3b8030cafdda99e235b3910c791c5e24
harry123001 <singhharkirat9090@gmail.com>
2023-11-27 13:41:27 -0500

    FizzBuzz practice

---
+++ Labs/fizzbuzz.py
@@ -0,0 +1,15 @@
+print('Enter a number upto 100!!')
+
+
+number = int(input())
+
+
+if (number % 3 == 0 and number % 5 == 0):
+    print("fizzbuzz")
+elif (number % 5 == 0):
+    print("buzz")
+elif ( number % 3 == 0):
+    print("fizz")
+else : 
+    (number)
+
